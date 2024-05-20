7f571c41c102544125be87d3d21dcce5f53849e9
Duc Huy Nguyen <duc-huy.nguyen@universite-paris-saclay.fr>
2024-05-20 10:03:10 +0200

    fix 3

--- .eslintrc.js
+++ .eslintrc.js
@@ -2,7 +2,8 @@ module.exports = {
   "env": {
     "browser": true,
     "es6": true,
-    "jest/globals": true
+    "jest/globals": true,
+    "node": true
   },
   "extends": [
     "eslint:recommended",
@@ -23,10 +24,6 @@ module.exports = {
       "error",
       2
     ],
-    "semi": [
-      "error",
-      "never"
-    ],
     "eqeqeq": "error",
     "no-trailing-spaces": "error",
     "object-curly-spacing": [
@@ -35,7 +32,6 @@ module.exports = {
     "arrow-spacing": [
       "error", { "before": true, "after": true }
     ],
-    "no-console": "error",
     "react/prop-types": 0
   }
 }
